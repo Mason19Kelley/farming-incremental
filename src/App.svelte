b0b8e6a52a39857f423522c184e63877f4811cbe
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-15 20:42:49 -0600

    added basic saving

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,27 @@
 	import Header from './components/Header.svelte';
 
   import { resources } from "./stores/resources";
+  import { onMount } from 'svelte';
 
   const incrementPotatoes  = () => {
       $resources.potatoes += $resources.potatoRate;
       $resources.carrots += $resources.carrotRate;
+      localStorage.setItem("save",convertResourcesToJSON());    
   }
 
+    onMount(() => {
+      const data = JSON.parse(localStorage.getItem("save"));
+      if(data) {
+        resources.set(data)
+      }
+      
+    });
+
+  function convertResourcesToJSON() {
+    const storeValue = $resources; // Use the $ prefix to access the current value of the store
+    const jsonString = JSON.stringify(storeValue, null, 2);
+    return jsonString
+  }
 
   setInterval(incrementPotatoes, 1000);
 </script>
